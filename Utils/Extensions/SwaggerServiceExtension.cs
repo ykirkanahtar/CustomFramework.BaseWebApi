8bfc8da2b327333dab7397dcb50630dce6b50f10
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2020-01-27 16:48:18 +0300

    Ufak düzenlemeler yapıldı

--- Utils/Extensions/SwaggerServiceExtension.cs
+++ Utils/Extensions/SwaggerServiceExtension.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace CustomFramework.BaseWebApi.Utils.Extensions
